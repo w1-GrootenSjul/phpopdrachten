111c3b5de81af0e225e4243967413f7ada6f9eda
Sjul Grooten <Sjul.Grooten@edu-kw1c.nl>
2020-02-13 09:30:37 +0100

    index.php en opdracht_2-1.php toegevoegd

--- index.php
+++ index.php
@@ -15,29 +15,76 @@
         <meta name="description" content="...">
         <meta name="keywords" content="...">
         <meta charset="utf-8">
-        <link href="css/style.css">
+        <link href="css/style.css" type="text/css" rel="stylesheet">
     </head>
     <body>
         <header>
             <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
         </header>
-        <aside>
-            <h2>Menu</h2>
-            <ul>
-                <li>Hoofdstuk 2
-                    <ul>
-                        <li>
-                            <a href="hoofdstuk2/opdracht_2-1.php">Opdracht 2.1</a>
-                        </li>
-                        <li>
-                            <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 2.2</a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-        </aside>
-        <main id="wrapper">
-            <h2>Uitwerkingen</h2>
-        </main>
+        <div>
+            <aside>
+                <h2>Menu</h2>
+                <ul>
+                    <li>Hoofdstuk 2
+                        <ul>
+                            <li>
+                                <a href="hoofdstuk2/opdracht_2-1.php">Opdracht 2.1</a>
+                            </li>
+                            <li>
+                                <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 2.2</a>
+                            </li>
+                        </ul>
+                    </li><br>
+                    <li>Hoofdstuk 3
+                        <ul>
+                            <li>
+                                <a href="hoofdstuk3/opdracht_3-1.php">Opdracht 3.1</a>
+                            </li>
+                            <li>
+                                <a href="hoofdstuk3/opdracht_3-2.php">Opdracht 3.2</a>
+                            </li>
+                            <li>
+                                <a href="hoofdstuk3/opdracht_3-3.php">Opdracht 3.3</a>
+                            </li>
+                        </ul>
+                    </li><br>
+                    <li>Hoofdstuk 4
+                        <ul>
+                            <li>
+                                <a href="hoofdstuk4/opdracht_4-1.php">Opdracht 4.1</a>
+                            </li>
+                            <li>
+                                <a href="hoofdstuk4/opdracht_4-2.php">Opdracht 4.2</a>
+                            </li>
+                            <li>
+                                <a href="hoofdstuk4/opdracht_4-3.php">Opdracht 4.3</a>
+                            </li>
+                            <li>
+                                <a href="hoofdstuk4/opdracht_4-4.php">Opdracht 4.4</a>
+                            </li>
+                        </ul>
+                    </li><br>
+                    <li>Hoofdstuk 5
+                        <ul>
+                            <li>
+                                <a href="hoofdstuk5/opdracht_5-1.php">Opdracht 5.1</a>
+                            </li>
+                            <li>
+                                <a href="hoofdstuk5/opdracht_5-2.php">Opdracht 5.2</a>
+                            </li>
+                            <li>
+                                <a href="hoofdstuk5/opdracht_5-3.php">Opdracht 5.3</a>
+                            </li>
+                            <li>
+                                <a href="hoofdstuk5/opdracht_5-4.php">Opdracht 5.4</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </aside>
+            <main id="wrapper">
+                <h2>Uitwerkingen</h2>
+            </main>
+        </div>
     </body>
 </html>
